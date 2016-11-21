eaf0b6f804d9ee363cc501cf0ef95274cf162ff2
Jackson Bailey <jackson.bailey@equifax.com>
2016-11-21 17:51:29 -0500

    Rearrange tests

--- src/test/java/io/github/jacksonbailey/ascii85/Ascii85Test.java
+++ src/test/java/io/github/jacksonbailey/ascii85/Ascii85Test.java
@@ -36,18 +36,6 @@ public class Ascii85Test {
   }
 
   @Test
-  public void basicDecodeTest() {
-    String encodedString =
-        "9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,"
-            + "O<DJ+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKY"
-            + "i(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIa"
-            + "l(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G"
-            + ">uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c";
-
-    assertEquals(SAMPLE_TEXT, new String(Ascii85.decode(encodedString), StandardCharsets.US_ASCII));
-  }
-
-  @Test
   public void decodeShouldIgnoreNewLineCharacter() {
     String encodedString =
         "9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,\n"
@@ -72,7 +60,19 @@ public class Ascii85Test {
   }
 
   @Test
-  public void basicEncodeTest() {
+  public void decodeParagraphTest() {
+    String encodedString =
+        "9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,"
+            + "O<DJ+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKY"
+            + "i(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIa"
+            + "l(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G"
+            + ">uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c";
+
+    assertEquals(SAMPLE_TEXT, new String(Ascii85.decode(encodedString), StandardCharsets.US_ASCII));
+  }
+
+  @Test
+  public void encodeParagraphTest() {
     String solution = "9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,"
         + "O<DJ+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKY"
         + "i(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIa"
