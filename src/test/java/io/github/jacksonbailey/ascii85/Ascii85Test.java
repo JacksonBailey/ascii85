db83d51b81308e2fc7d2d3f38af3f99d8fa99c0c
Jackson Bailey <jackson.bailey@equifax.com>
2016-11-22 10:34:23 -0500

    Make tests easier to read

--- src/test/java/io/github/jacksonbailey/ascii85/Ascii85Test.java
+++ src/test/java/io/github/jacksonbailey/ascii85/Ascii85Test.java
@@ -11,11 +11,6 @@ import org.junit.Test;
  */
 public class Ascii85Test {
 
-  private static final String SAMPLE_TEXT = "Man is distinguished, not only by his reason, but by "
-      + "this singular passion from other animals, which is a lust of the mind, that by a "
-      + "perseverance of delight in the continued and indefatigable generation of knowledge, "
-      + "exceeds the short vehemence of any carnal pleasure.";
-
   @Test(expected = IllegalArgumentException.class)
   public void decodeShouldFailOnNullInput() {
     Ascii85.decode(null);
@@ -92,25 +87,45 @@ public class Ascii85Test {
 
   @Test
   public void decodeParagraphTest() {
-    String encodedString =
-        "9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,"
-            + "O<DJ+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKY"
-            + "i(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIa"
-            + "l(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G"
-            + ">uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c";
 
-    assertEquals(SAMPLE_TEXT, new String(Ascii85.decode(encodedString), StandardCharsets.US_ASCII));
+    String encoded = "9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,O<DJ"
+        + "+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKYi(DIb:@FD,*)+C]"
+        + "U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIal(DId<j@<?3r@:F%a+D58'ATD4$B"
+        + "l@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G>uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c";
+
+    byte[] output = Ascii85.decode(encoded);
+
+    String text = "Man is distinguished, not only by his reason, but by this singular passion from "
+        + "other animals, which is a lust of the mind, that by a perseverance of delight in the "
+        + "continued and indefatigable generation of knowledge, exceeds the short vehemence of any "
+        + "carnal pleasure.";
+
+    byte[] expectedOutput = asciiBytesFromString(text);
+
+    assertArrayEquals(expectedOutput, output);
   }
 
   @Test
   public void encodeParagraphTest() {
-    String solution = "9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,"
-        + "O<DJ+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKY"
-        + "i(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIa"
-        + "l(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G"
-        + ">uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c";
+    String text = "Man is distinguished, not only by his reason, but by this singular passion from "
+        + "other animals, which is a lust of the mind, that by a perseverance of delight in the "
+        + "continued and indefatigable generation of knowledge, exceeds the short vehemence of any "
+        + "carnal pleasure.";
 
-    assertEquals(solution, Ascii85.encode(SAMPLE_TEXT.getBytes()));
+    byte[] input = asciiBytesFromString(text);
+
+    String output = Ascii85.encode(input);
+
+    String encoded = "9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,O<DJ"
+        + "+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKYi(DIb:@FD,*)+C]"
+        + "U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIal(DId<j@<?3r@:F%a+D58'ATD4$B"
+        + "l@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G>uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c";
+
+    assertEquals(encoded, output);
+  }
+
+  private static byte[] asciiBytesFromString(String s) {
+    return s.getBytes(StandardCharsets.US_ASCII);
   }
 
 }
